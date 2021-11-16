61fc50096af117a811708c7f98e7a749a4a06a01
Sam Black <44275925+PsiOffenseUp@users.noreply.github.com>
2021-11-15 16:53:30 -0800

    Started on UI

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,107 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.panel5 = new System.Windows.Forms.Panel();
+			this.consoleTextTemplate = new System.Windows.Forms.Label();
+			this.consolePanel = new System.Windows.Forms.FlowLayoutPanel();
+			this.panel2.SuspendLayout();
+			this.consolePanel.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(240)))), ((int)(((byte)(253)))));
+			this.panel1.Location = new System.Drawing.Point(3, 51);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(458, 552);
+			this.panel1.TabIndex = 0;
+			// 
+			// panel2
+			// 
+			this.panel2.Controls.Add(this.label1);
+			this.panel2.Controls.Add(this.panel1);
+			this.panel2.Location = new System.Drawing.Point(12, 12);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(464, 606);
+			this.panel2.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Doppio One", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(253)))), ((int)(((byte)(253)))));
+			this.label1.Location = new System.Drawing.Point(3, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(196, 26);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Hyperparameters";
+			// 
+			// panel3
+			// 
+			this.panel3.Location = new System.Drawing.Point(482, 12);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(562, 55);
+			this.panel3.TabIndex = 2;
+			// 
+			// panel5
+			// 
+			this.panel5.Location = new System.Drawing.Point(482, 74);
+			this.panel5.Name = "panel5";
+			this.panel5.Size = new System.Drawing.Size(562, 369);
+			this.panel5.TabIndex = 4;
+			// 
+			// consoleTextTemplate
+			// 
+			this.consoleTextTemplate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.consoleTextTemplate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(30)))), ((int)(((byte)(40)))));
+			this.consoleTextTemplate.Location = new System.Drawing.Point(3, 0);
+			this.consoleTextTemplate.Name = "consoleTextTemplate";
+			this.consoleTextTemplate.Size = new System.Drawing.Size(100, 23);
+			this.consoleTextTemplate.TabIndex = 0;
+			this.consoleTextTemplate.Text = "label2";
+			// 
+			// consolePanel
+			// 
+			this.consolePanel.AutoScroll = true;
+			this.consolePanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(3)))), ((int)(((byte)(12)))));
+			this.consolePanel.Controls.Add(this.consoleTextTemplate);
+			this.consolePanel.Location = new System.Drawing.Point(482, 449);
+			this.consolePanel.Name = "consolePanel";
+			this.consolePanel.Size = new System.Drawing.Size(562, 169);
+			this.consolePanel.TabIndex = 1;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(20)))), ((int)(((byte)(26)))));
+			this.ClientSize = new System.Drawing.Size(1056, 630);
+			this.Controls.Add(this.consolePanel);
+			this.Controls.Add(this.panel5);
+			this.Controls.Add(this.panel3);
+			this.Controls.Add(this.panel2);
+			this.Name = "Form1";
+			this.Text = "CNNF Accuracy";
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			this.consolePanel.ResumeLayout(false);
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.Label consoleTextTemplate;
+		private System.Windows.Forms.Panel panel5;
+		private System.Windows.Forms.FlowLayoutPanel consolePanel;
 	}
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,42 @@ namespace CNNF_Accuracy
 		{
 			InitializeComponent();
 		}
+
+		#region Console Text Methods
+
+		static readonly Color consoleMessageColor = Color.FromArgb(70, 240, 80);
+		static readonly Color consoleErrorColor = Color.FromArgb(250, 30, 40);
+		static readonly Font consoleFont = new Font(FontFamily.GenericMonospace, 10.0f, FontStyle.Regular);
+
+		/// <summary>  Puts text into the console panel </summary>
+		/// <param name="message"></param>
+		void ConsolePrint(string message)
+		{
+			Label label = new Label()
+			{
+				Text = message,
+				BackColor = consoleMessageColor,
+				Font = consoleFont
+			};
+
+			consolePanel.Controls.Add(label); //Add the newly created label to the console
+		}
+
+		/// <summary> Puts text into the console panel as an error</summary>
+		void ConsoleErrorPrint(string message)
+		{
+			Label label = new Label()
+			{
+				Text = message,
+				BackColor = consoleErrorColor,
+				Font = consoleFont
+			};
+
+			consolePanel.Controls.Add(label); //Add the newly created label to the console
+		}
+
+		#endregion
 	}
+
+
 }
